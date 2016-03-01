4b7a635d85c587221dfe9a22b0407bcd360cb86f
Peter Hitchcock <phitchcock@me.com>
2016-03-01 14:19:03 -0800

    rebuild gemspec

--- lib/redux.rb
+++ lib/redux.rb
@@ -3,6 +3,7 @@ require "redux/version"
 module Redux
   class Application
     def call(env)
+      'echo debug > debug.txt';
       [200, {'Content-Type' => 'text/html'},
         ["Hello from Redux!"]]
     end
